6256eae42a436e92fc5cbaac195089c803e9dba6
Qone2 <whrbdnjs33@naver.com>
2023-03-06 23:34:25 +0900

    문제: 백준 11650 좌표 정렬하기

---
+++ boj/silver/11650 좌표 정렬하기/main.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    coords = []
+    for _ in range(N):
+        coords.append(tuple(map(int, input().split())))
+    coords.sort(key=lambda coord: coord[1])
+    coords.sort()
+    for coord in coords:
+        print(coord[0], end=' ')
+        print(coord[1])
